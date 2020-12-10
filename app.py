e77ed24e352bb3258fa25065d318aee573d4d8ea
brendo712 <brendo712@gmail.com>
2020-12-10 07:45:31 -0500

    Created models and added user routes with login functionality

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_cors import CORS
+from resources.users import user
+from resources.trips import trip
+from resources.comments import comment
+import models
+from flask_login import LoginManager
+
 
 DEBUG = True
 PORT = 3001
 
 app = Flask(__name__)
+
+app.secret_key = "Capstone"
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
 
 @app.route('/')
 def index():
---
+++ models.py
@@ -0,0 +1,69 @@
+from peewee import *
+import datetime
+from flask_login import UserMixin
+
+DATABASE = SqliteDatabase('itinerary.sqlite')
+
+class User(UserMixin, Model):
+    username=CharField(unique=True)
+    email=CharField(unique=True)
+    password=CharField()
+    first_name=CharField()
+    last_name=CharField()
+    join_date= DateTimeField()
+
+    class Meta:
+        database = DATABASE
+
+
+class Destination(Model):
+    name=CharField()
+
+    class Meta:
+        database = DATABASE
+
+
+class Activity(Model):
+    name=CharField()
+    description=TextField()
+
+    class Meta:
+        database = DATABASE
+
+
+class Trip(Model):
+    title=CharField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+    destination=ForeignKeyField(Destination, backref='trips')
+    activities=ForeignKeyField(Activity, backref='trips')
+    comment=ForeignKeyField(Comment, backref='trips')
+    length=IntegerField()
+
+    class Meta:
+        database = DATABASE
+
+
+class Comment(Model):
+    user = ForeignKeyField(User, backref='comments')
+    trip = ForeignKeyField(Trip, backref='comments')
+    body = TextField()
+    send_date = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+
+
+class Relationship(BaseModel):
+    from_user=ForeignKeyField(User, backref='relationships')
+    to_user=ForeignKeyField(User, backref='related_to')
+
+    class Meta:
+        database = DATABASE
+        indexes = ((('from_user', 'to_user'), True),)
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User, Trip, Destination, Activity, Comment, Relationship], safe=True)
+    print("TABLES Created")
+    DATABASE.close()
